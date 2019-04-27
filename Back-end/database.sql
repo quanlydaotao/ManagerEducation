69e13da057b0a40d6a2114a246e546aed666dabf
Đào Huy Đức <ducanime1@gmail.com>
2019-04-28 04:09:27 +0700

    update db
    a

--- Back-end/database.sql
+++ Back-end/database.sql
@@ -1,13 +1,13 @@
--- MySQL dump 10.13  Distrib 5.7.24, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.15, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: genNCKH
+-- Host: localhost    Database: manage
 -- ------------------------------------------------------
--- Server version	5.7.24-0ubuntu0.18.04.1
+-- Server version	8.0.15
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+ SET NAMES utf8 ;
 /*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
 /*!40103 SET TIME_ZONE='+00:00' */;
 /*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
@@ -21,7 +21,7 @@
 
 DROP TABLE IF EXISTS `authority`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `authority` (
   `name` varchar(50) NOT NULL,
   PRIMARY KEY (`name`)
@@ -34,146 +34,8 @@ CREATE TABLE `authority` (
 
 LOCK TABLES `authority` WRITE;
 /*!40000 ALTER TABLE `authority` DISABLE KEYS */;
-INSERT INTO `authority` VALUES ('ROLE_ADMIN'),('ROLE_USER');
+INSERT INTO `authority` VALUES ('ROLE_ADMIN'),('ROLE_PARENTS'),('ROLE_STUDENT'),('ROLE_TEACHER');
 /*!40000 ALTER TABLE `authority` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `categorys`
---
-
-DROP TABLE IF EXISTS `categorys`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `categorys` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `parent_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_categorys_parent_id` (`parent_id`),
-  CONSTRAINT `fk_categorys_parent_id` FOREIGN KEY (`parent_id`) REFERENCES `categorys` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `categorys`
---
-
-LOCK TABLES `categorys` WRITE;
-/*!40000 ALTER TABLE `categorys` DISABLE KEYS */;
-/*!40000 ALTER TABLE `categorys` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `customer`
---
-
-DROP TABLE IF EXISTS `customer`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `customer` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `email` varchar(255) DEFAULT NULL,
-  `name` varchar(255) DEFAULT NULL,
-  `create_at` datetime,
-  `phone` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `customer`
---
-
-LOCK TABLES `customer` WRITE;
-/*!40000 ALTER TABLE `customer` DISABLE KEYS */;
-/*!40000 ALTER TABLE `customer` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `orders`
---
-
-DROP TABLE IF EXISTS `orders`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `orders` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `create_at` datetime,
-  `customer_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_orders_customer_id` (`customer_id`),
-  CONSTRAINT `fk_orders_customer_id` FOREIGN KEY (`customer_id`) REFERENCES `customer` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `orders`
---
-
-LOCK TABLES `orders` WRITE;
-/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
-/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `orders_product`
---
-
-DROP TABLE IF EXISTS `orders_product`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `orders_product` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `quantity` int(11) DEFAULT NULL,
-  `product_id` bigint(20) DEFAULT NULL,
-  `order_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_orders_product_product_id` (`product_id`),
-  KEY `fk_orders_product_order_id` (`order_id`),
-  CONSTRAINT `fk_orders_product_order_id` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`),
-  CONSTRAINT `fk_orders_product_product_id` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `orders_product`
---
-
-LOCK TABLES `orders_product` WRITE;
-/*!40000 ALTER TABLE `orders_product` DISABLE KEYS */;
-/*!40000 ALTER TABLE `orders_product` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `product`
---
-
-DROP TABLE IF EXISTS `product`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `product` (
-  `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
-  `price` bigint(20) DEFAULT NULL,
-  `quantity` int(11) DEFAULT NULL,
-  `create_at` datetime,
-  `update_at` datetime,
-  `img` varchar(255) DEFAULT NULL,
-  `category_id` bigint(20) DEFAULT NULL,
-  PRIMARY KEY (`id`),
-  KEY `fk_product_category_id` (`category_id`),
-  CONSTRAINT `fk_product_category_id` FOREIGN KEY (`category_id`) REFERENCES `categorys` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `product`
---
-
-LOCK TABLES `product` WRITE;
-/*!40000 ALTER TABLE `product` DISABLE KEYS */;
-/*!40000 ALTER TABLE `product` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -182,24 +44,31 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `user` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `login` varchar(50) NOT NULL,
   `password_hash` varchar(60) NOT NULL,
-  `first_name` varchar(50) DEFAULT NULL,
-  `last_name` varchar(50) DEFAULT NULL,
+  `first_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `last_name` varchar(50) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   `email` varchar(254) DEFAULT NULL,
   `image_url` varchar(256) DEFAULT NULL,
   `activated` bit(1) NOT NULL,
   `lang_key` varchar(6) DEFAULT NULL,
   `activation_key` varchar(20) DEFAULT NULL,
   `reset_key` varchar(20) DEFAULT NULL,
-  `created_by` varchar(50) NOT NULL,
+  `address` varchar(254) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
+  `phone_number` varchar(20) DEFAULT NULL,
+  `identity_card_number` varchar(20) DEFAULT NULL,
+  `birthday` date DEFAULT NULL,
+  `sex` bit(1) DEFAULT b'1',
+  `nations` varchar(50) DEFAULT NULL,
+  `address1` varchar(254) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `ux_user_login` (`login`),
+  UNIQUE KEY `user_identity_card_number_uindex` (`identity_card_number`),
   UNIQUE KEY `ux_user_email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -208,7 +77,7 @@ CREATE TABLE `user` (
 
 LOCK TABLES `user` WRITE;
 /*!40000 ALTER TABLE `user` DISABLE KEYS */;
-INSERT INTO `user` VALUES (1,'system','$2a$10$mE.qmcV0mFU5NcKh73TZx.z4ueI/.bDWbj0T1BYyqP481kGGarKLG','System','System','system@localhost','',_binary '','en',NULL,NULL,'system'),(2,'anonymoususer','$2a$10$j8S5d7Sr7.8VTOYNviDPOeWX8KcYILUVJBsYV83Y5NtECayypx9lO','Anonymous','User','anonymous@localhost','',_binary '','en',NULL,NULL,'system'),(3,'AD00001','$2a$10$gSAhZrxMllrbgj/kkK9UceBPpChGWJA7SYIb1Mqo.n5aNLq1/oRrC','Administrator','Administrator','admin@localhost','',_binary '','en',NULL,NULL,'system'),(4,'user','$2a$10$VEjxo0jq2YG9Rbk2HmX9S.k1uZBGYUHdUcid3g/vfiEl7lwWgOH/K','User','User','user@localhost','',_binary '','en',NULL,NULL,'system');
+INSERT INTO `user` VALUES (1,'GV00001','$2a$10$mUM7YmKTsSCjXhSrSN8lFuh5oWKznS2L9CJ3kcxjAOQIkZjzNhCju','Ngô Mai','Anh','hoangnhu@gmail.com','',_binary '','en',NULL,NULL,'Thanh Hóa','0363205500','175077296','0000-00-00',_binary '','kinh','Thanh Hóa'),(2,'PH00001','$2a$10$mUM7YmKTsSCjXhSrSN8lFuh5oWKznS2L9CJ3kcxjAOQIkZjzNhCju','Nguyễn Minh','Nhật','caothuha@gmail.com','',_binary '','en',NULL,NULL,'Hà Nội','0363205501','175077297','2019-04-27',_binary '','kinh','Hà Nội'),(3,'AD00001','$2a$10$mUM7YmKTsSCjXhSrSN8lFuh5oWKznS2L9CJ3kcxjAOQIkZjzNhCju','Administrator','Administrator','admin@localhost','',_binary '','en',NULL,NULL,'Hà Nam','0363205502','175077299','2019-04-27',_binary '','kinh','Lào Cai'),(4,'HV00001','$2a$10$mUM7YmKTsSCjXhSrSN8lFuh5oWKznS2L9CJ3kcxjAOQIkZjzNhCju','Lê Văn','Hải','nhatminh@gmail.com','',_binary '','en',NULL,NULL,'Nam Định','0363205503','175077295','2019-04-27',_binary '\0','kinh','Hà Nội'),(5,'AD00002','$2a$10$mUM7YmKTsSCjXhSrSN8lFuh5oWKznS2L9CJ3kcxjAOQIkZjzNhCju','Đào Huy','Đức','ducanime1@gmail.com','',_binary '\0','en',NULL,NULL,'Thái Bình','0363205504','175077291','2019-04-27',_binary '','kinh','Thanh Hóa');
 /*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -218,7 +87,7 @@ UNLOCK TABLES;
 
 DROP TABLE IF EXISTS `user_authority`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
+ SET character_set_client = utf8mb4 ;
 CREATE TABLE `user_authority` (
   `user_id` bigint(20) NOT NULL,
   `authority_name` varchar(50) NOT NULL,
@@ -235,7 +104,7 @@ CREATE TABLE `user_authority` (
 
 LOCK TABLES `user_authority` WRITE;
 /*!40000 ALTER TABLE `user_authority` DISABLE KEYS */;
-INSERT INTO `user_authority` VALUES (1,'ROLE_ADMIN'),(3,'ROLE_ADMIN'),(1,'ROLE_USER'),(3,'ROLE_USER'),(4,'ROLE_USER');
+INSERT INTO `user_authority` VALUES (3,'ROLE_ADMIN'),(5,'ROLE_ADMIN'),(2,'ROLE_PARENTS'),(4,'ROLE_STUDENT'),(1,'ROLE_TEACHER');
 /*!40000 ALTER TABLE `user_authority` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -248,4 +117,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2019-01-06 15:21:17
+-- Dump completed on 2019-04-28  4:08:36
